498338d2f2ea1e5e12b74be83ccde576abbd7475
vcashk <vcashk@gmail.com>
2015-10-04 11:44:24 +0400

    assignment20

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Vikash Khanna on 10/3/15.
+//  Copyright (c) 2015 Vikash Khanna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
